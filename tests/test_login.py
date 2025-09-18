10f6925b026e3cf0519f4406e667d909f9306ace
Harsh3790 <harshchauhan3790>
2025-09-18 12:43:44 +0530

    Added nagative testing on test_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,3 +6,9 @@ def test_valid_login(driver):
     login_page.open()
     login_page.login("standard_user", "secret_sauce")
     assert "inventory" in driver.current_url
+    
+def test_invalid_login(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login("wrong_user", "wrong_pass")
+    assert "inventory" not in driver.current_url
