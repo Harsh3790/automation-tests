563b09fa61cb480efd0b9a5833f660e763333bc2
Harsh3790 <harshchauhan3790>
2025-09-18 17:10:52 +0530

    add invalid password test case

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,3 +12,9 @@ def test_invalid_login(driver):
     login_page.open()
     login_page.login("wrong_user", "wrong_pass")
     assert "inventory" not in driver.current_url
+
+def test_invalid_password(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
+    login_page.login("standard_user","wrong_pass")
+    assert "Epic sadface" in driver.page_source
